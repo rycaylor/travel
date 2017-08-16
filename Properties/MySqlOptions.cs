58d3f32e751a23901df90e7ffe89c09b464cc208
Kyle <kyle.mcfar1291@gmail.com>
2017-08-16 13:12:59 -0700

    obscured DB connection string

--- Properties/MySqlOptions.cs
+++ Properties/MySqlOptions.cs
@@ -1,4 +1,6 @@
-namespace travel
-{
-    
+namespace travel {
+    public class MySqlOptions {
+        public string Name { get; set; }
+        public string ConnectionString { get; set; }
+    }
 }
